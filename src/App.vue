7752d9605a7e6873dd54ad207a9d59e8f05fc609
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-03-22 22:43:18 +0100

    setting header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="app">
-    CIAO
-  </div>
+  <body id="app">
+
+    <FableHeader />
+
+  </body>
 </template>
 
 <script>
+import FableHeader from './components/macro/FableHeader.vue'
 
 export default {
   name: 'App',
   components: {
+    FableHeader,
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/scss/_common.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  background-color: #4a6e9f;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/macro/FableHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+
+        <div class="container">
+            <!-- logo -->
+            <figure class="menu__logo">
+                <img src="../../assets/img/logo.png" alt="logo">
+            </figure>
+
+            <div>
+                
+            </div>
+        </div>
+        
+    </header>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style lang="scss" scoped>
+
+header{
+  
+    
+   
+    background-color: white;
+
+    .menu__logo img{
+        max-height: 120px;
+    }
+}
+
+</style>
